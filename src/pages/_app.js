4503a2176a40ab8fcca3eecfa53b7f1242f9331b
Bunghez Ovidiu <ovidiueduard@gmail.com>
2023-05-13 16:06:23 +0300

    feat(gitlab): first commit

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import 'components/styles/globals.css';
-import 'components/styles/bootstrap/css/bootstrap-grid.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
